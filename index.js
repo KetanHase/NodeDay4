ffc6d2d08bc38c2639f9911678363532a671dcef
KetanHase <ketanhase999@gmail.com>
2025-07-18 12:02:35 +0530

    Final

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ const express = require('express');
 const app = express();
 
 
-// Middleware
-app.use(express.json());
+app.get('/',(req,res)=>{
+    res.render('home.ejs')
+})
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('Hello, Express! ketan 123');
-});  
-  
-// Start server     
-const PORT = 5000;
-const HOST = 127.;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.get('/about',(req,res)=>{
+        res.render('about.ejs')
+})
+
+app.get('/contact',(req,res)=>{
+    res.render('contact.ejs')
+})
+
+app.set('view engine', 'ejs');
+
+
+
+
+const PORT=5000
+const HOST='127.0.0.1'
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port http://${HOST}:${PORT}`)
+})
