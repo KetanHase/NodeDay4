d2975204caa755fed6c9056348af0a4bfa6454ba
KetanHase <ketanhase999@gmail.com>
2025-07-18 11:31:25 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+  
+// Start server     
+const PORT = 5000;
+const HOST = 127.;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
